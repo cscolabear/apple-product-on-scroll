0bcbbafb5de779d9d2cc27386064e2ee8cec0f06
Cola <cscolabear@gmail.com>
2020-04-27 11:47:41 +0800

    refactor for resize

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-const $color_section = $('.color-section');
-const color_section_top = $color_section.offset().top;
-const color_section_bottom = $color_section.offset().top + $color_section.outerHeight(true);
 const $product_list = $('.product._2, .product._3');
-
-const $color_section2 = $('.color-section-2');
-const color_section_top2 = $color_section2.offset().top;
-const color_section_bottom2 = $color_section2.offset().top + $color_section2.outerHeight(true);
 const $product_list2 = $('.product2._2, .product2._3');
 
-const getViewDetail = () => {
-  const view_top = $(window).scrollTop();
-  const view_height = $(window).height();
-  const view_bottom = view_top + view_height;
+const getEleDetail = ($ele) => {
+  const top = $ele.offset().top;
+  const height = $ele.outerHeight(true);
+  const bottom = top + height;
 
   return {
-    view_top, view_height, view_bottom
+    top, height, bottom
+  };
+}
+
+const getViewDetail = () => {
+  const top = $(window).scrollTop();
+  const height = $(window).height();
+  const bottom = top + height;
+
+  return {
+    top, height, bottom
   };
 }
 
@@ -31,31 +34,32 @@ const setClipPath = ($ele, inset) => {
 }
 
 const scrollTopMonitor = () => {
+  const ele_detail = getEleDetail($('.color-section'));
   const view_detail = getViewDetail();
 
   //
-  if (view_detail.view_top < color_section_top) {
+  if (view_detail.top < ele_detail.top) {
     const inset = 'inset(100% 0px 0px 0px)';
     setClipPath($product_list, inset);
   }
 
   //
-  if (view_detail.view_bottom > color_section_bottom) {
+  if (view_detail.bottom > ele_detail.bottom) {
     const inset = 'inset(0% 0px 0px 0px)';
     setClipPath($product_list, inset);
   }
 
-  if (view_detail.view_top >= color_section_top &&
-    view_detail.view_bottom <= color_section_bottom) {
+  if (view_detail.top >= ele_detail.top &&
+    view_detail.bottom <= ele_detail.bottom) {
     // console.log('in sec: ' + view_top);
 
     $product_list.each((index, ele) => {
       // 第 index 層 section  bottom 位置
-      const current_section_bottom = (color_section_top + (view_detail.view_height * (index + 1)));
+      const current_section_bottom = (ele_detail.top + (view_detail.height * (index + 1)));
       // console.log($ele.attr('class') + ': ' + color_section_bottom);
 
       // 目前 view_top 在該層佔多少百分比
-      let inset_value = (current_section_bottom - view_detail.view_top) / view_detail.view_height * 100;
+      let inset_value = (current_section_bottom - view_detail.top) / view_detail.height * 100;
       // 極值保護: 不小於 0，不大於 100
       inset_value =
         Math.min(
@@ -72,10 +76,11 @@ window.requestAnimationFrame(scrollTopMonitor);
 
 
 const scrollTopMonitor2 = () => {
+  const ele_detail = getEleDetail($('.color-section-2'));
   const view_detail = getViewDetail();
 
   //
-  if (view_detail.view_top < color_section_top2) {
+  if (view_detail.top < ele_detail.top) {
     const inset = 'inset(0px 100% 0px 0px)';
     if ($product_list2.css('clip-path') !== inset) {
       setClipPath($product_list2, inset);
@@ -83,22 +88,22 @@ const scrollTopMonitor2 = () => {
   }
 
   //
-  if (view_detail.view_bottom > color_section_bottom2) {
+  if (view_detail.bottom > ele_detail.bottom) {
     const inset = 'inset(0px 0% 0px 0px)';
     setClipPath($product_list2, inset);
   }
 
-  if (view_detail.view_top >= color_section_top2 &&
-    view_detail.view_bottom <= color_section_bottom2) {
-    // console.log('in sec: ' + view_top);
+  if (view_detail.top >= ele_detail.top &&
+    view_detail.bottom <= ele_detail.bottom) {
+      // console.log('in sec: ' + view_detail.top);
 
     $product_list2.each((index, ele) => {
       // 第 index 層 section  bottom 位置
-      const current_section_bottom2 = (color_section_top2 + (view_detail.view_height * (index + 1)));
+      const current_section_bottom = (ele_detail.top + (view_detail.height * (index + 1)));
       // console.log($(ele).attr('class') + ': ' + current_section_bottom2);
 
       // 目前 view_top 在該層佔多少百分比
-      let inset_value = (current_section_bottom2 - view_detail.view_top) / view_detail.view_height * 100;
+      let inset_value = (current_section_bottom - view_detail.top) / view_detail.height * 100;
       // 極值保護: 不小於 0，不大於 100
       inset_value =
         Math.min(
